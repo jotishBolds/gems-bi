b33c6d099570e982d2d473de9b0253062fd12ed9
jotishBolds <jotish.dev@bolds.in>
2025-01-21 15:45:10 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['3ueks2jaxjpgpjeg.public.blob.vercel-storage.com']
+  },
   async headers() {
     return [
       {
